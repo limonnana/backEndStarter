dc22467819b9a47937ded7ef2ce40f0dcc939034
eyal rosenzvaig <rosenzvaig@gmail.com>
2024-01-15 15:54:12 +0200

    security that works

--- src/main/java/com/limonnana/be/BeApplication.java
+++ src/main/java/com/limonnana/be/BeApplication.java
@@ -2,17 +2,14 @@ package com.limonnana.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 
-@ComponentScan("com.limonnana.be.repository")
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
+
+@SpringBootApplication
 public class BeApplication {
 
     public static void main(String[] args) {
@@ -20,15 +17,7 @@ public class BeApplication {
     }
 
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/users").allowedOrigins("http://localhost:4200");
-            }
-        };
-    }
-
-
 }
+
+
+
--- src/main/java/com/limonnana/be/config/Config.java
+++ src/main/java/com/limonnana/be/config/Config.java
@@ -1,35 +1,38 @@
 package com.limonnana.be.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Config {
 
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/my-account", "/my-balance", "/my-loans", "/my-cards").authenticated()
+                .requestMatchers("/contact", "/notices", "/api/v1/users/users").permitAll())
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
 
-
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        return encoder;
+    PasswordEncoder noOpPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
+
+
 }
